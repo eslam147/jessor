cf99a5cb54d93a1248173a73991e2d5311641686
Abdelaziz <zeseoudi@gmail.com>
2024-07-31 14:15:19 +0300

    remove backslash not used

--- resources/views/layouts/footer_js.blade.php
+++ resources/views/layouts/footer_js.blade.php
@@ -1,48 +1,48 @@
-<script src="{{ url('/assets/js/vendor.bundle.base.js') }}"></script>
-<script src="{{ url('/assets/js/Chart.min.js') }}"></script>
-<script src="{{ url('/assets/js/jquery.validate.min.js') }}"></script>
-<script src="{{ url('/assets/jquery-toast-plugin/jquery.toast.min.js') }}"></script>
-<script src="{{ url('/assets/select2/select2.min.js') }}"></script>
+<script src="{{ url('assets/js/vendor.bundle.base.js') }}"></script>
+<script src="{{ url('assets/js/Chart.min.js') }}"></script>
+<script src="{{ url('assets/js/jquery.validate.min.js') }}"></script>
+<script src="{{ url('assets/jquery-toast-plugin/jquery.toast.min.js') }}"></script>
+<script src="{{ url('assets/select2/select2.min.js') }}"></script>
 
-<script src="{{ url('/assets/js/off-canvas.js') }}"></script>
-<script src="{{ url('/assets/js/hoverable-collapse.js') }}"></script>
-<script src="{{ url('/assets/js/misc.js') }}"></script>
-<script src="{{ url('/assets/js/settings.js') }}"></script>
-<script src="{{ url('/assets/js/todolist.js') }}"></script>
-<script src="{{ url('/assets/js/ekko-lightbox.min.js') }}"></script>
-
-
-<script src="{{ url('/assets/bootstrap-table/bootstrap-table.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/bootstrap-table-mobile.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/bootstrap-table-export.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/fixed-columns.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/tableExport.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/jspdf.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/jspdf.plugin.autotable.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/jquery.tablednd.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/reorder-rows.min.js') }}"></script>
-<script src="{{ url('/assets/bootstrap-table/loadash.min.js') }}"></script>
+<script src="{{ url('assets/js/off-canvas.js') }}"></script>
+<script src="{{ url('assets/js/hoverable-collapse.js') }}"></script>
+<script src="{{ url('assets/js/misc.js') }}"></script>
+<script src="{{ url('assets/js/settings.js') }}"></script>
+<script src="{{ url('assets/js/todolist.js') }}"></script>
+<script src="{{ url('assets/js/ekko-lightbox.min.js') }}"></script>
 
 
-<script src="{{ url('/assets/js/jquery.cookie.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/bootstrap-table.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/bootstrap-table-mobile.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/bootstrap-table-export.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/fixed-columns.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/tableExport.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/jspdf.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/jspdf.plugin.autotable.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/jquery.tablednd.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/reorder-rows.min.js') }}"></script>
+<script src="{{ url('assets/bootstrap-table/loadash.min.js') }}"></script>
+
+
+<script src="{{ url('assets/js/jquery.cookie.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.12.3/sweetalert2.all.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js"></script>
-<script src="{{ url('/assets/js/datepicker.min.js') }}"></script>
-<script src="{{ url('/assets/js/daterangepicker.js') }}"></script>
-<script src="{{ url('/assets/js/jquery.repeater.js') }}"></script>
-<script src="{{ url('/assets/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ url('assets/js/datepicker.min.js') }}"></script>
+<script src="{{ url('assets/js/daterangepicker.js') }}"></script>
+<script src="{{ url('assets/js/jquery.repeater.js') }}"></script>
+<script src="{{ url('assets/tinymce/tinymce.min.js') }}"></script>
 
-<script src="{{ url('/assets/color-picker/jquery-asColor.min.js') }}"></script>
-<script src="{{ url('/assets/color-picker/color.min.js') }}"></script>
+<script src="{{ url('assets/color-picker/jquery-asColor.min.js') }}"></script>
+<script src="{{ url('assets/color-picker/color.min.js') }}"></script>
 
-<script src="{{ url('/assets/js/custom/validate.js') }}"></script>
-<script src="{{ url('/assets/js/jquery-additional-methods.min.js') }}"></script>
-<script src="{{ url('/assets/js/custom/function.js') }}?v=1.0.4""></script>
-<script src="{{ url('/assets/js/custom/custom.js') }}?v=1.0.5"></script>
-<script src="{{ url('/assets/js/custom/custom-bootstrap-table.js') }}"></script>
+<script src="{{ url('assets/js/custom/validate.js') }}"></script>
+<script src="{{ url('assets/js/jquery-additional-methods.min.js') }}"></script>
+<script src="{{ url('assets/js/custom/function.js') }}?v=1.0.4""></script>
+<script src="{{ url('assets/js/custom/custom.js') }}?v=1.0.5"></script>
+<script src="{{ url('assets/js/custom/custom-bootstrap-table.js') }}"></script>
 
-<script src="{{ url('/assets/ckeditor-4/ckeditor.js') }}"></script>
-<script src="{{ url('/assets/ckeditor-4/adapters/jquery.js') }}" async></script>
+<script src="{{ url('assets/ckeditor-4/ckeditor.js') }}"></script>
+<script src="{{ url('assets/ckeditor-4/adapters/jquery.js') }}" async></script>
 
 @if ($errors->any())
     @foreach ($errors->all() as $error)
