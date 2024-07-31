1c7603c65864f6d519df40d0f64095cc88e9ff7d
Abdelaziz <zeseoudi@gmail.com>
2024-07-31 18:51:54 +0300

    Add Private Browser Error For File

--- resources/views/student_dashboard/files/index.blade.php
+++ resources/views/student_dashboard/files/index.blade.php
@@ -6,6 +6,7 @@
             width: 100%;
             height: 500px;
         }
+
         .vtabs .tab-content iframe {
             height: 400px;
         }
@@ -41,53 +42,81 @@
                                                 </ul>
                                                 <!-- Tab panes -->
                                                 <div class="tab-content">
-                                                    <div class="tab-pane active" id="home4" role="tabpanel">
-                                                        @foreach ($topic->file as $row)
-                                                            <div class="row"style="width: -webkit-fill-available;">
-                                                                <div class="col-12"style="width: -webkit-fill-available;">
-                                                                    @if ($row->isYoutubeVideo())
-                                                                        <div id="player" data-plyr-provider="youtube"
-                                                                            data-plyr-embed-id="{{ getYouTubeVideoId($row->file_url) }}">
+                                                    @foreach ($videos as $row)
+                                                        <div class="row"style="width: -webkit-fill-available;">
+                                                            <div class="col-12"style="width: -webkit-fill-available;">
+                                                                @if ($row->isYoutubeVideo())
+                                                                    <div id="player" data-plyr-provider="youtube"
+                                                                        data-plyr-embed-id="{{ getYouTubeVideoId($row->file_url) }}">
+                                                                    </div>
+                                                                @elseif($row->isVideoCorner() || $row->isVideoCornerDownload())
+                                                                    @if (trim(request()->userAgent()) == 'semi_browser_by_adel')
+                                                                        @if ($row->isVideoCornerDownload())
+                                                                            <input type="hidden" id="title_for_browser"
+                                                                                value="{{ $row->file_name }}">
+                                                                            <input type="hidden" id="download_for_browser"
+                                                                                value="{{ $row->video_download_link }}">
+                                                                        @endif
+                                                                        <iframe src="{{ $row->file_url }}"
+                                                                            style="width: -webkit-fill-available;"
+                                                                            frameborder="0"></iframe>
+                                                                    @else
+                                                                        <div
+                                                                            style="text-align: center; margin-top: 30px; margin-bottom: 130px;">
+                                                                            <p
+                                                                                style="direction: rtl; font-family: cairo; font-size: 30px; margin-bottom: 13px;">
+                                                                                هذا المتصفح غير مصرح به. يرجى استخدام
+                                                                                المتصفح المسموح.
+                                                                            </p>
+
+                                                                            <a href="https://infinityschool.net/tenancy/assets/browser/InfinitySchoolV1.exe"
+                                                                                target="_blank" rel="noopener noreferrer"
+                                                                                class="btn btn-primary"
+                                                                                style="margin: auto;display: inline-block;border: 0;padding: 0;">
+                                                                                <button type="button"
+                                                                                    class="btn btn-primary"
+                                                                                    style="border: solid 5px; padding: 10px 20px; font-size: 18px;">
+                                                                                    <i class="fas fa-download"></i> Download
+                                                                                </button>
+                                                                            </a>
                                                                         </div>
-                                                                    @elseif($row->isVideoCorner())
-                                                                        <input type="hidden" id="title_for_browser" value="{{ $row->file_name }}">
-                                                                        <input type="hidden" id="download_for_browser" value="{{ $row->download_link }}">
-                                                                        <iframe src="{{ $row->file_url }}" class="" style="width: -webkit-fill-available;"frameborder="0"></iframe>
                                                                     @endif
-                                                                </div>
+                                                                @endif
+
                                                             </div>
-                                                        @endforeach
-                                                    </div>
-                                                    <div class="tab-pane" id="profile4" role="tabpanel">
-                                                        <div class="p-15">
-                                                            <h4>Fusce porta eros a nisl varius, non molestie metus mollis.
-                                                                Pellentesque tincidunt ante sit amet ornare lacinia.</h4>
-                                                            <p>Duis cursus eros lorem, pretium ornare purus tincidunt
-                                                                eleifend. Etiam quis justo vitae erat faucibus pharetra.
-                                                                Morbi in ullamcorper diam. Morbi lacinia, sem vitae
-                                                                dignissim cursus, massa nibh semper magna, nec pellentesque
-                                                                lorem nisl quis ex.</p>
-                                                            <h3>Donec vitae laoreet neque, id convallis ante.</h3>
                                                         </div>
+                                                    @endforeach
+                                                </div>
+                                                <div class="tab-pane" id="profile4" role="tabpanel">
+                                                    <div class="p-15">
+                                                        <h4>Fusce porta eros a nisl varius, non molestie metus mollis.
+                                                            Pellentesque tincidunt ante sit amet ornare lacinia.</h4>
+                                                        <p>Duis cursus eros lorem, pretium ornare purus tincidunt
+                                                            eleifend. Etiam quis justo vitae erat faucibus pharetra.
+                                                            Morbi in ullamcorper diam. Morbi lacinia, sem vitae
+                                                            dignissim cursus, massa nibh semper magna, nec pellentesque
+                                                            lorem nisl quis ex.</p>
+                                                        <h3>Donec vitae laoreet neque, id convallis ante.</h3>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div id="navpills2-2" class="tab-pane">
-                                    Files
-                                </div>
-                                <div id="navpills2-3" class="tab-pane">
-                                    Videos
-                                </div>
+                            </div>
+                            <div id="navpills2-2" class="tab-pane">
+                                Files
+                            </div>
+                            <div id="navpills2-3" class="tab-pane">
+                                Videos
                             </div>
                         </div>
-                        <!-- /.box-body -->
                     </div>
+                    <!-- /.box-body -->
                 </div>
-            </section>
         </div>
+        </section>
+    </div>
     </div>
 @endsection
 @section('script')
