101887c9ba6a86a77e03a9c2e07002e0bdd68c96
Abdelaziz <zeseoudi@gmail.com>
2024-08-15 20:13:35 +0300

    update Section Resource

--- app/Http/Resources/Student/SectionResource.php
+++ app/Http/Resources/Student/SectionResource.php
@@ -6,18 +6,11 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class SectionResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'class_school_id' => $this->class_school_id
+            'name' => $this->name
         ];
     }
 }
