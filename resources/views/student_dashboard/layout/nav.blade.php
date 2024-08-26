645ca007c407fd12528378dfbbf1ca2b7562e393
Abdelaziz <zeseoudi@gmail.com>
2024-08-26 21:34:54 +0300

    some updates

--- resources/views/student_dashboard/layout/nav.blade.php
+++ resources/views/student_dashboard/layout/nav.blade.php
@@ -1,8 +1,10 @@
 <header class="main-header">
-    <div class="d-flex align-items-center logo-box justify-content-start">
-        <a href="#" class="waves-effect waves-light nav-link d-none d-md-inline-block mx-10 push-btn bg-transparent" data-toggle="push-menu" role="button">
-			<span class="icon-Align-left"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
-		</a>
+    <div class="d-flex align-items-center d-none d-md-block logo-box justify-content-start">
+        <a href="#" class="waves-effect waves-light nav-link d-none d-md-inline-block mx-10 push-btn bg-transparent"
+            data-toggle="push-menu" role="button">
+            <span class="icon-Align-left"><span class="path1"></span><span class="path2"></span><span
+                    class="path3"></span></span>
+        </a>
         <!-- Logo -->
         <a href="index.html" class="logo">
             <!-- logo-->
