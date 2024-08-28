8dbc0e169773ad1392339489b2dc879cfd921b34
Abdelaziz <zeseoudi@gmail.com>
2024-08-28 17:14:56 +0300

    update domain tenant

--- app/Http/Controllers/centeral/DomainController.php
+++ app/Http/Controllers/centeral/DomainController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\centeral;
 
+use Exception;
 use App\Models\Tenant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Stancl\Tenancy\Commands\Rollback;
 use Illuminate\Support\Facades\Artisan;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -13,26 +16,36 @@ class DomainController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'subdomain' => "required|unique:tenants|min:4|max:50|regex:^[a-zA-Z0-9]+$"
+            'subdomain' => "required|unique:tenants,id|min:4|max:50|regex:/^[a-zA-Z0-9]+$/"
         ]);
-        $tenant= Tenant::find($request->subdomain);
-        if($tenant){
-            Alert::warning('Warning','this domain already exist');
-            return redirect()->back();
-        }else{
-            $tanent = Tenant::create(['id' => $request->subdomain]);
-            $tanent->domains()->create(['domain' => $request->subdomain .'.localhost']);
+        DB::beginTransaction();
 
-            // remove old dummy data
+        try {
+            $tenant = Tenant::find($request->subdomain);
+            if ($tenant) {
+                Alert::warning('Warning', 'This domain already exists');
+                return redirect()->back();
+            }
+
+            $tenant = Tenant::create(['id' => $request->subdomain]);
+            $tenant->domains()->create(['domain' => $request->subdomain . '.localhost']);
+
             Artisan::call('tenants:migrate-fresh', [
                 '--tenants' => $request->subdomain
             ]);
-            // add defult data from seed
             Artisan::call('tenants:seed', [
                 '--tenants' => $request->subdomain
             ]);
-            Alert::success('Congratulations','Tenant Created Successfully');
+
+            DB::commit();
+
+            Alert::success('Congratulations', 'Tenant Created Successfully');
             return redirect()->back();
+        } catch (Exception $e) {
+            DB::rollBack();
+            Alert::error('Error', 'An error occurred: ' . $e->getMessage());
+            return redirect()->back()->withInput();
         }
     }
+
 }
