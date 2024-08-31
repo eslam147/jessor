54f544ceb01c07af7147989cf00493c688bbb7b4
Abdelaziz <zeseoudi@gmail.com>
2024-08-31 18:13:45 +0300

    remove un used style

--- resources/views/student_dashboard/subject/index.blade.php
+++ resources/views/student_dashboard/subject/index.blade.php
@@ -1,46 +1,4 @@
 @extends('student_dashboard.layout.app')
-@section('style')
-    <style>
-        /* .bg-green {
-                            background-color: #6AC977;
-                            color: #ffffff;
-                            border-radius: 15px;
-                        }
-
-                        .bg-green-br {
-                            border-color: #6ac977;
-                            color: #7E8FC1;
-                            background-color: transparent;
-                            border-radius: 15px;
-                        } */
-
-        /* .no_image_available::before {
-                            content: "";
-                            position: absolute;
-                            background: url("{{ settingByType('logo1') ? tenant_asset(settingByType('logo1')) : global_asset('assets/logo.svg') }}");
-                            z-index: 99;
-                            top: 50%;
-                            left: 50%;
-                            transform: translate(-50%, -50%);
-                            height: 65%;
-                            width: 100%;
-                            background-size: cover;
-                            background-position: center;
-                            backdrop-filter: grayscale(4);
-                        }
-
-                        .no_image_available::after {
-                            content: "";
-                            position: absolute;
-                            width: 100%;
-                            background: #efefef;
-                            z-index: 9;
-                            top: 0;
-                            left: 0;
-                            height: 100%;
-                        } */
-    </style>
-@endsection
 @section('content')
     <div class="content-wrapper">
         <div class="container-full">
@@ -55,8 +13,8 @@
                                     data-overlay-light="9">
                                     <div class="d-lg-flex align-items-center justify-content-between">
                                         <div class="d-md-flex align-items-center mb-30 mb-lg-0 w-p100">
-                                            <img style="background-color: #{{ $row->bg_color }}66" src="{{ !empty($row->image) ? $row->image : loadTenantMainAsset('logo1', global_asset('images/no_image_available.jpg')) }}"
-                                                class="h-100 img-fluid object-fit-cover w-150 " alt="">
+                                            <img style="background-color: {{ $row->bg_color }}" src="{{ !empty($row->image) ? $row->image : loadTenantMainAsset('logo1', global_asset('images/no_image_available.jpg')) }}"
+                                                class="img-fluid object-fit-cover w-150 p-10" alt="">
                                             <div class="ms-30 w-75">
                                                 <h4 class="mb-10 text-bold">{{ $row->name }}</h4>
                                                 @if ($row->teachersViaSubject->count())
